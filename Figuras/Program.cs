f6539cfdaca129786037a16eb074cb86bc3ad685
Mario Garcia Valdez <mariosky@gmail.com>
2020-11-26 12:41:17 -0800

    Campos agregados
    Esto resuelve el  closes issue #1, por favor @JoseCarlosPaezV  revisa que esté bien.

--- Figuras/Program.cs
+++ Figuras/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace Figuras
 {
+
+    class Figura
+        {
+        public int xPos, yPos;
+            
+        }
     class Program
     {
         static void Main(string[] args)
